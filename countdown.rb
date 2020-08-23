c4d1202eaaa08c8f7375f41d803c26e3c7c7a7b4
Janae Quinones <janaemassa@gmail.com>
2020-08-23 06:45:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(number)
-  puts "#{number} SECOND(S)!"
   while number > 0
-    number -= 1
     puts "#{number} SECOND(S)!"
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
